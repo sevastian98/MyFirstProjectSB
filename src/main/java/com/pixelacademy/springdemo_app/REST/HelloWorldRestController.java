4bb28bcabbe9028d9054812834ac941143f16364
Sevastian Vascan <sevastian.vascan7@gmail.com>
2025-05-14 20:32:30 +0300

    Add GetMapping

--- src/main/java/com/pixelacademy/springdemo_app/REST/HelloWorldRestController.java
+++ src/main/java/com/pixelacademy/springdemo_app/REST/HelloWorldRestController.java
@@ -1,4 +1,12 @@
 package com.pixelacademy.springdemo_app.REST;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController // adnotare
 public class HelloWorldRestController {
+    @GetMapping("/")
+    public String sayHello() {
+        return "Hello world";
+    }
 }
