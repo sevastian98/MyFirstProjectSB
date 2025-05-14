28a771b8cf2e57140b075e6fa092c105336520ec
Sevastian Vascan <sevastian.vascan7@gmail.com>
2025-05-14 20:37:36 +0300

    Add endpoint meniu

--- src/main/java/com/pixelacademy/springdemo_app/REST/HelloWorldRestController.java
+++ src/main/java/com/pixelacademy/springdemo_app/REST/HelloWorldRestController.java
@@ -5,8 +5,13 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController // adnotare
 public class HelloWorldRestController {
+
     @GetMapping("/")
     public String sayHello() {
         return "Hello world";
+    }
+    @GetMapping("/meniu")
+    public String meniu() {
+        return  "1.pizza \n2.Sarmale \n3.Zama";
     }
 }
